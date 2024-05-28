8a97a54b6d8037306c02b230b07f418a565102be
ChauQuang1111 <148325990+ChauQuang1111@users.noreply.github.com>
2024-05-28 13:48:58 +0700

    first commit

---
+++ src/RequestProduct.java
@@ -0,0 +1,11 @@
+
+// Interface: requestProduct
+interface RequestProduct {
+    void setPriority(String priority);
+
+    void setExpire(String expireDay);
+
+    void setStatus(String status);
+
+    void processRequest();
+}
